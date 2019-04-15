d5acb549bab4675bb0248f039cf7a536ece76658
18858375431 <18858375431@163.com>
2019-04-15 09:02:23 +0800

    delete

--- hy/security/src/main/java/com/cmcc/hy/commons/Interfaces/HttpInterface.java
+++
@@ -1,9 +0,0 @@
-package com.cmcc.hy.commons.Interfaces;
-
-public interface HttpInterface {
-    String PROTOCOL="http://";
-    String IP="172.28.22.47";
-    String PORT="23456";
-    String PATH="/security/";
-    String URL=PROTOCOL+IP+":"+PORT+PATH;
-}
